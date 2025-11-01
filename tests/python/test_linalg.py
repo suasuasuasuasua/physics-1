f1ffbd52b9d1db7c4868e62fd363f97cff021b7b
Justin Hoang <j124.dev@proton.me>
2025-11-01 09:54:42 -0600

    test(linalg): add default constructor tests

--- tests/python/test_linalg.py
+++ tests/python/test_linalg.py
@@ -3,6 +3,18 @@ from __future__ import annotations
 from physics_1 import linalg
 
 
+def test_Vector2D_DefaultConstructor():
+    x = linalg.Vector2D()
+
+    assert x.x() == 0 and x.y() == 0
+
+
+def test_Vector2D_Constructor():
+    x = linalg.Vector2D(1, 10)
+
+    assert x.x() == 1 and x.y() == 10
+
+
 def test_Vector2D_Add():
     x = linalg.Vector2D(1, 2)
     y = linalg.Vector2D(3, 5)
