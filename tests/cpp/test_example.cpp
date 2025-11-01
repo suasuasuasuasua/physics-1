72edc024bb7b0e70c3f3ab702c7c05a656f096bb
Justin Hoang <j124.dev@proton.me>
2025-11-01 16:41:51 -0600

    test(cpp): add mul test and nest in anon namespace

--- tests/cpp/test_example.cpp
+++ tests/cpp/test_example.cpp
@@ -1,6 +1,7 @@
 #include <example/functions.h>
 #include <gtest/gtest.h>
 
+namespace {
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
@@ -9,12 +10,18 @@ TEST(HelloTest, BasicAssertions) {
   EXPECT_EQ(7 * 6, 42);
 }
 
-TEST(AddTest, BasicFunctionAssertions) {
+TEST(FunctionTest, Add) {
   EXPECT_EQ(example::add(10, 4), 14);
   EXPECT_EQ(example::add(1234, 412341234), 412342468);
 }
 
-TEST(SubTest, BasicFunctionAssertions) {
+TEST(FunctionTest, Sub) {
   EXPECT_EQ(example::sub(10, 4), 6);
   EXPECT_EQ(example::sub(1234, 412341234), -412340000);
 }
+
+TEST(FunctionTest, Mul) {
+  EXPECT_EQ(example::mul(10, 4), 40);
+  EXPECT_EQ(example::mul(123, 456), 56088);
+}
+}  // namespace
