d1feab15cf58910dc4fb72b95bf076265cf20a98
Justin Hoang <j124.dev@proton.me>
2025-11-04 21:08:17 -0700

    fix(python): update stubs for functions

--- python/physics_1/example/_example/functions.pyi
+++ python/physics_1/example/_example/functions.pyi
@@ -3,16 +3,20 @@ Some example functions demoing pybind11
 """
 from __future__ import annotations
 import typing
-__all__: list[str] = ['add', 'mul', 'sub']
-def add(i: typing.SupportsInt, j: typing.SupportsInt) -> int:
+__all__: list[str] = ['add', 'div', 'mul', 'sub']
+def add(i: typing.SupportsFloat, j: typing.SupportsFloat) -> float:
     """
             Add two numbers
     """
-def mul(i: typing.SupportsInt, j: typing.SupportsInt) -> int:
+def div(i: typing.SupportsFloat, j: typing.SupportsFloat) -> float:
+    """
+            Divide two numbers
+    """
+def mul(i: typing.SupportsFloat, j: typing.SupportsFloat) -> float:
     """
             Multiply two numbers
     """
-def sub(i: typing.SupportsInt, j: typing.SupportsInt) -> int:
+def sub(i: typing.SupportsFloat, j: typing.SupportsFloat) -> float:
     """
             Subtract two numbers
     """
