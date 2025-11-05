7148551869b1930f76cc8f4374dbacf6666c0b35
Justin Hoang <j124.dev@proton.me>
2025-11-04 19:49:38 -0700

    refactor(python): use doubles uniformly

--- inc/example/functions.h
+++ inc/example/functions.h
@@ -6,8 +6,8 @@
 namespace py = pybind11;
 
 namespace example::functions {
-int add(int i, int j);
-int sub(int i, int j);
+double add(double i, double j);
+double sub(double i, double j);
 double mul(double i, double j);
 double div(double i, double j);
 }  // namespace example::functions
--- src/example/functions.cpp
+++ src/example/functions.cpp
@@ -8,8 +8,8 @@
 namespace py = pybind11;
 
 namespace example::functions {
-int add(int i, int j) { return i + j; }
-int sub(int i, int j) { return i - j; }
+double add(double i, double j) { return i + j; }
+double sub(double i, double j) { return i - j; }
 double mul(double i, double j) { return i * j; }
 double div(double i, double j) {
   if (j == 0.0) {
