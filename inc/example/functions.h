ed764ec3e4ae6024b47f6228b7835cae9891a49e
Justin Hoang <j124.dev@proton.me>
2025-11-05 21:02:03 -0700

    fix(python): reduce dependency on heavier python modules

--- inc/example/functions.h
+++ inc/example/functions.h
@@ -1,10 +1,6 @@
 #ifndef INCLUDE_EXAMPLE_FUNCTIONS_H_
 #define INCLUDE_EXAMPLE_FUNCTIONS_H_
 
-#include <pybind11/pybind11.h>
-
-namespace py = pybind11;
-
 namespace example::functions {
 double add(double i, double j);
 double sub(double i, double j);
